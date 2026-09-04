6aceb4a770c284b9cc975eeaa72b9605bd643cac
Tigrex <Tigrex@WIN-MVSURE7PTJD>
2026-09-04 19:31:02 +0300

    Довёл до билда. Доделал колбеки, протокол сигналинга, сценарий маршрутизации

--- Source/Application/main/Runtime.h
+++ Source/Application/main/Runtime.h
@@ -6,20 +6,20 @@
 #include <memory>
 #include <iostream>
 #include <functional>
-#include <map>
-#include <queue>
+#include <deque>
 #include "boost/json.hpp"
 
-#include <cmath>
+#include <random>
 
-#include<csignal>
+class RelayServer;
+class NodeRuntime;
 
 namespace Wyvern {
-    class Runtime {
+    class Runtime : public std::enable_shared_from_this<Runtime> {
         std::shared_ptr < boost::asio::io_context > ioc;
         boost::asio::executor_work_guard<boost::asio::io_context::executor_type> relayThreadHolder;
-        std::unique_ptr<RelayServer> server;
-        std::unique_ptr<NodeRuntime> nodeRuntime;
+        std::shared_ptr<RelayServer> server;
+        std::shared_ptr<NodeRuntime> nodeRuntime;
 
         bool isNeedSetupRelay;
         bool isNeedSetupNodeRuntime;
@@ -32,10 +32,11 @@ namespace Wyvern {
             isNeedSetupRelay(false),
             isNeedSetupNodeRuntime(true)
         {
+
             parse_arguments(argc, argv);
 
             setupRelay(9005);
-            setupNodeRuntime();
+            setupNodeRuntime(ioc);
 
             ioc->run();//Не делаю ioc->stop(). Пусть умрет при SIGINT
         }
@@ -56,18 +57,34 @@ namespace Wyvern {
         }
 
         inline void setupRelay(uint16_t port) {
-            if (isNeedSetupRelay) server = std::make_unique<RelayServer> (port);
+
+            if (isNeedSetupRelay) server = std::make_shared<RelayServer> (port);
         }
-        inline void setupNodeRuntime() {
-            if (isNeedSetupNodeRuntime) nodeRuntime = std::make_unique<NodeRuntime>();
+        inline void setupNodeRuntime(auto ioc) {
+            if (isNeedSetupNodeRuntime) nodeRuntime = std::make_shared<NodeRuntime>(ioc);
         }
     };
 }
 
-namespace Utilities {
+namespace Wyvern::Utilities {
     static inline std::string normalize_path(std::string p) {
         if (!p.empty() && p.front() == '/') p.erase(p.begin());
         return p;
+    }
+
+    static inline std::string generateRandNumSeq(int seqSize) {
+        static std::random_device rd;
+        static std::mt19937 gen(rd());
+        static std::uniform_int_distribution<int> digit(0, 9);
+        static std::string s;
+
+        s = "";
+
+        for (int i = 0; i < seqSize; i++) {
+            s += char(digit(gen));
+        }
+
+        return s;
     }
 };
 
@@ -93,10 +110,10 @@ class RelayServer {
 
 	std::unordered_map<std::string ,Node> storedNodes;
 
-	bool storeDelayedMessage(Node, boost::json::object);
-	bool changeNodeActivity(Node, NodeActivityStatus);
+	bool storeDelayedMessage(Node, boost::json::object) { return true; };
+	bool changeNodeActivity(Node, NodeActivityStatus) { return true; };
 
-	bool isNodeActive(Node);
+    bool isNodeActive(Node) { return true; };
 
 public:
 	explicit RelayServer(uint16_t port) {
@@ -112,16 +129,11 @@ public:
 			});
 	}
 
-	bool setupServer(boost::asio::ip::address endpoint);
-	void serverWorker();
-
-	void onReqest(std::function<void>);
-
 private:
     void onIncoming(std::shared_ptr<rtc::WebSocket> ws) {//TODO: Вынести в cpp, проверить код
         // handshake ещё не закончен
         ws->onOpen([this, ws] {
-            const std::string id = Utilities::normalize_path(ws->path().value_or(""));
+            const std::string id = Wyvern::Utilities::normalize_path(ws->path().value_or(""));
             if (id.empty()) {
                 ws->close();
                 return;
@@ -140,7 +152,7 @@ private:
             route(ws, std::get<rtc::string>(std::move(msg)));
             });
 
-        ws->onClosed([this, ws] {
+        ws->onClosed([this, ws] {//TODO: Вынести это в отдельную логику?
             std::lock_guard lk{ mtx_ };
             for (auto it = storedNodes.begin(); it != storedNodes.end(); ) {
                 if (it->second.connection == ws) it = storedNodes.erase(it);
@@ -196,8 +208,8 @@ namespace Wyvern {
     public:
         Configuration() {};
 
-        RelaySpecific getPreferRelaySpecific();
-        P2PConnectionType getPreferConnectionType();
+        RelaySpecific getPreferRelaySpecific() { return RelaySpecific::Closest; };
+        P2PConnectionType getPreferConnectionType() { return P2PConnectionType::PreferRelay; };
         
         int getRelayTimeoutMS() { return 500; } // TODO: заглушка
         int getMaxReconnectAttempts() { return 5; }
@@ -208,17 +220,22 @@ namespace Wyvern {
     public:
 
 
-        std::vector<std::string> getRelayNames();//TODO:Уточнить цель функции и изменить имя
-        std::string chooseRelayBy(RelaySpecific);
-        std::string chooseNodeType();
+        std::string getSelfID() {
+            static std::string localID = "SOME-KIND-OF-ID-" + Wyvern::Utilities::generateRandNumSeq(4);//TODO: Заглушка
+            return localID;
+        }
 
-        std::vector < std::string> requestKnownNodesFromRelay();
+        //std::vector<std::string> getRelayNames();//TODO:Уточнить цель функции и изменить имя
+        std::string getRelayBy(RelaySpecific) { return "0.0.0.0"; };//Выбор ссылки не реле по какому-то признаку
+        std::string getNodeType() { return ""; };//Надо уточнить
+
+        std::vector < std::string> requestKnownNodesFromRelay() { return std::vector<std::string> {}; };
     };
 }
 
 class RelayConnection {
     std::shared_ptr<boost::asio::io_context> ioc;
-    boost::asio::steady_timer timeoutTimer;
+    //boost::asio::steady_timer timeoutTimer; // TODO: На будущее для реконекта
 
     std::shared_ptr<rtc::WebSocket> connection;
 
@@ -228,14 +245,12 @@ class RelayConnection {
     std::shared_ptr<Wyvern::Configuration> applicationConfig;
     std::shared_ptr <Wyvern::ConnectionInformationService> InformationService;
 
-      
-
 
     void setupCallbacks() {
         connection->onOpen([this] {
             boost::asio::post(*ioc, [this] {
                 isConnectedToRelay = true;
-                timeoutTimer.cancel();
+                //timeoutTimer.cancel();
                 });
 
             });
@@ -254,10 +269,6 @@ class RelayConnection {
             });
     }
 
-    void onSignal(std::string) {//Заглушка
-        return;
-    }
-
 public:
     RelayConnection(
         std::shared_ptr<boost::asio::io_context> ioContext,
@@ -269,7 +280,7 @@ public:
         applicationConfig(config),
         connection(std::make_shared<rtc::WebSocket>()),
         ioc(ioContext),
-        timeoutTimer(*ioc, boost::asio::chrono::seconds(applicationConfig->getRelayTimeoutMS())),
+        //timeoutTimer(*ioc, boost::asio::chrono::mileseconds(applicationConfig->getRelayTimeoutMS())),
         isConnectedToRelay(false) 
     {
         setupCallbacks();
@@ -277,129 +288,195 @@ public:
 
 
     //Делаем реле подключение отдельно
-    void requestConnectToRelay() {//TODO: В private? 
-        connection->open("ws://" + InformationService->chooseRelayBy(applicationConfig->getPreferRelaySpecific()) + "/" + getSelfID());
-
-        timeoutTimer.expires_after(std::chrono::seconds(
-            applicationConfig->getRelayTimeoutMS()));
-        timeoutTimer.async_wait([this](boost::system::error_code ec) {
-            if (ec || stopping) return;          // cancel
-            if (!connection->isOpen()) {
-                connection->close();
-                scheduleReconnect();
-            }
-            });
-    }
-
-    std::function<void()> connectAttemptCallback;//TODO: В private
-    std::function<void()> reconnectFailedCallback;//TODO: В private
-
-
-    void onReconnectAttempt(std::function<void()> callback = NULL) {
-        connectAttemptCallback = std::move(callback);
-    }
-
-    void doConnectAttempt() {
-        static int attempt = 0;
-
-        if (connectAttemptCallback) connectAttemptCallback();
-        printf("Попытка подключиться...\n");// TODO: заглушка
-
-        auto delay = std::chrono::milliseconds(500 * (1 << std::min(attempt, 5)));//Интересный механизм от нейронки, пока о таком не думал, но пусть будет
-        ++attempt;
-    }
-
-    void onReconnectFailed(std::function<void()> callback = NULL) {
-        reconnectFailedCallback = std::move(callback);
-    }
-
-
-    bool requestDirectConnection(std::string remoteID, std::string remoteAddres, std::string port) {//TODO: А надо ли вообще это?
-        connection->open("ws://" + remoteAddres + port + "/" + remoteID);// Прокидывать конфиг сразу без каких-то значений?
-        return connection->isOpen();
+    void requestConnectToRelay() {//TODO: В private?
+        if (!isConnectedToRelay)
+            connection->open("ws://" + InformationService->getRelayBy(applicationConfig->getPreferRelaySpecific()) + "/" + InformationService->getSelfID());
     }
 
 
     void requestInfoAboutRelay() {};//Запрос ближайшего известного relay у подключенных пиров. (А надо ли это? Есть ли такая ситуация, когда подключение есть, а реле нет?)
 
-
-};
-
-class NodeConnection {
-
-	rtc::Configuration config;
-
-	std::shared_ptr<rtc::PeerConnection> pc;
-	std::shared_ptr<rtc::DataChannel> dc;
-
-
-
-
-	void loop();
-
 public:
-    NodeConnection() :
-            pc(std::make_shared<rtc::PeerConnection>(config))
-    {};
+    void sendSignal(const std::string& toNodeId, boost::json::object msg) {
+        if (!connection || !connection->isOpen()) return;
 
-    inline void connect() {
-        pc->createDataChannel("dataChannelLabel");//TODO: заглушка названия
+        msg["from"] = InformationService->getSelfID();
+        msg["to"] = toNodeId;
+
+        std::string serialized = boost::json::serialize(msg);
+        connection->send(serialized);
     }
 
+    // Передаем incoming сообщения наверх в NodeRuntime
+    void setSignalCallback(std::function<void(const std::string&)> cb) {
+        onSignalCb = std::move(cb);
+    }
+
+private:
+    std::function<void(const std::string&)> onSignalCb;
+
+    void onSignal(std::string body) {
+        if (onSignalCb) onSignalCb(body);
+    }
 };
 
-class NodeRuntime {//Сюда писать бизнес логику?
 
+
+
+
+class NodeConnection : public std::enable_shared_from_this<NodeConnection> {
+    rtc::Configuration config;
+    std::shared_ptr<rtc::PeerConnection> pc;
+    std::shared_ptr<rtc::DataChannel> dc;
+
+    inline std::string generateName() {
+        return "TEST-NAME-" + Wyvern::Utilities::generateRandNumSeq(4);
+    }
+
+public:
+    NodeConnection() : pc(std::make_shared<rtc::PeerConnection>(config)) 
+    {
+
+    }
+
+
+    // Инициатор (Peer A): создает offer
+    void initAsOffer(std::function<void(const boost::json::object&)> sendSignalCb) {
+        setupCallbacks(sendSignalCb);
+        dc = pc->createDataChannel(generateName());
+        setupChannelCallbacks(dc);
+    }
+
+    // Принимающий (Peer B): ожидает offer и создает answer
+    void initAsAnswer(std::function<void(const boost::json::object&)> sendSignalCb) {
+        setupCallbacks(sendSignalCb);
+        pc->onDataChannel([this](std::shared_ptr<rtc::DataChannel> incomingDc) {
+            dc = incomingDc;
+            setupChannelCallbacks(dc);
+            });
+    }
+
+    // Обработка удаленного SDP (Offer или Answer)
+    void handleRemoteDescription(const std::string& type, const std::string& sdp) {
+        pc->setRemoteDescription(rtc::Description(sdp, type));
+    }
+
+    // Обработка удаленного ICE кандидата
+    void handleRemoteCandidate(const std::string& candidate, const std::string& mid) {
+        pc->addRemoteCandidate(rtc::Candidate(candidate, mid));
+    }
+
+private:
+    void setupCallbacks(std::function<void(const boost::json::object&)> sendSignalCb) {
+        // Локальное описания SDP готов (создан offer или answer)
+        pc->onLocalDescription([sendSignalCb](rtc::Description desc) {
+            boost::json::object payload;
+            payload["sdp"] = std::string(desc);
+            payload["type"] = desc.typeString();
+
+            boost::json::object msg;
+            msg["type"] = desc.typeString(); // "offer" или "answer"
+            msg["payload"] = payload;
+
+            sendSignalCb(msg);
+            });
+
+        // Найден новый локальный ICE кандидат (Trickle ICE)
+        pc->onLocalCandidate([sendSignalCb](rtc::Candidate cand) {
+            boost::json::object payload;
+            payload["candidate"] = cand.candidate();
+            payload["mid"] = cand.mid();
+
+            boost::json::object msg;
+            msg["type"] = "candidate";
+            msg["payload"] = payload;
+
+            sendSignalCb(msg);
+            });
+    }
+
+    void setupChannelCallbacks(std::shared_ptr<rtc::DataChannel> channel) {
+        channel->onOpen([]() {
+            std::cout << "[P2P] DataChannel успешно открыт!" << std::endl;
+            });
+
+        channel->onMessage([](rtc::message_variant msg) {
+            if (std::holds_alternative<rtc::string>(msg)) {
+                std::cout << "[P2P Msg] " << std::get<rtc::string>(msg) << std::endl;
+            }
+            });
+    }
+};
+
+class NodeRuntime : public std::enable_shared_from_this<NodeRuntime> {
+    std::shared_ptr<boost::asio::io_context> ioc;
     std::shared_ptr<Wyvern::Configuration> applicationConfig;
     std::shared_ptr<Wyvern::ConnectionInformationService> InformationService;
 
-    std::shared_ptr<boost::asio::io_context> ioc;
-
-    
-
+    std::unique_ptr<RelayConnection> relayConnection;
     std::unordered_map<std::string, std::shared_ptr<NodeConnection>> storedNodes;
 
-
-
-
-    void onSignal(std::string body) {
-        //TODO: Что-то?
-    }
-
-    std::string getSelfID() {
-        static std::string localID = "SOME-KIND-OF-ID";//TODO: Заглушка
-        return localID;
-    }
-
-    
-
 public:
-    NodeRuntime(std::shared_ptr<boost::asio::io_context> ioContext) :
-            ioc(ioContext),
-            InformationService(std::make_shared<Wyvern::ConnectionInformationService>()),
-            applicationConfig(std::make_shared<Wyvern::Configuration>())
+    NodeRuntime(std::shared_ptr<boost::asio::io_context> ioContext)
+        : ioc(ioContext),
+        InformationService(std::make_shared<Wyvern::ConnectionInformationService>()),
+        applicationConfig(std::make_shared<Wyvern::Configuration>())
     {
-        
-    };
+        relayConnection = std::make_unique<RelayConnection>(ioc, applicationConfig, InformationService);
+        relayConnection->setSignalCallback([this](const std::string& body) {
+            onSignal(body);
+            });
+        relayConnection->requestConnectToRelay();
+    }
 
-    RelayConnection relayConnection(ioc, applicationConfig, InformationService);
-    
-
-    bool requestConnectionToNode(std::string remoteID) {
-
-
-        static int num = 0;
-        
-        std::string someName = "SOME-ID-" + num++;//TODO: Заглушка
+    // Инициация подключения к удаленному пиру (Пир A)
+    void connectToPeer(const std::string& remoteID) {
         auto nodeCon = std::make_shared<NodeConnection>();
+        storedNodes[remoteID] = nodeCon;
 
-        if (true) {
-            storedNodes.insert(std::make_pair(someName, nodeCon));
+        nodeCon->initAsOffer([this, remoteID](const boost::json::object& signalMsg) {
+            relayConnection->sendSignal(remoteID, signalMsg);
+            });
+    }
 
-            nodeCon->connect();
+private:
+    // Парсинг входящего сигнала от Relay (Signaling Dispatcher)
+    void onSignal(const std::string& body) {
+        boost::system::error_code ec;
+        auto jv = boost::json::parse(body, ec);
+        if (ec || !jv.is_object()) return;
+
+        const auto& obj = jv.as_object();
+        std::string from = obj.at("from").as_string().c_str();
+        std::string type = obj.at("type").as_string().c_str();
+        const auto& payload = obj.at("payload").as_object();
+
+        // 1. Если это Offer, а у нас нет этого пира — создаем Responder (Пир B)
+        if (storedNodes.find(from) == storedNodes.end()) {
+            if (type == "offer") {
+                auto nodeCon = std::make_shared<NodeConnection>();
+                storedNodes[from] = nodeCon;
+
+                nodeCon->initAsAnswer([this, from](const boost::json::object& signalMsg) {
+                    relayConnection->sendSignal(from, signalMsg);
+                    });
+            }
+            else {
+                return; // Игнорируем кандидаты/answer от неизвестных инициаторов
+            }
         }
-        
-        
 
+        auto nodeCon = storedNodes[from];
+
+        // 2. Диспетчеризация по типам
+        if (type == "offer" || type == "answer") {//TODO: Нужна более строгая типизация
+            std::string sdp = payload.at("sdp").as_string().c_str();
+            nodeCon->handleRemoteDescription(type, sdp);
+        }
+        else if (type == "candidate") {
+            std::string cand = payload.at("candidate").as_string().c_str();
+            std::string mid = payload.at("mid").as_string().c_str();
+            nodeCon->handleRemoteCandidate(cand, mid);
+        }
     }
 };
